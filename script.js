2c12713e75fbfad1b4aa00471cef3069ee23536f
kuldeep kumar <kuldeeprssa1@gmail.com>
2026-02-14 17:54:07 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,76 @@
+let gameSeq = [];
+let userSeq = [];
+let btns = ["red", "yellow", "green", "purple"];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+// Start Game
+document.addEventListener("keypress", function () {
+    if (!started) {
+        started = true;
+        levelUp();
+    }
+});
+
+function gameFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(() => btn.classList.remove("flash"), 250);
+}
+
+function userFlash(btn) {
+    btn.classList.add("userflash");
+    setTimeout(() => btn.classList.remove("userflash"), 200);
+}
+
+function levelUp() {
+    userSeq = [];
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random() * 4);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`#${randColor}`);
+
+    gameSeq.push(randColor);
+    gameFlash(randBtn);
+}
+
+function checkAnswer(idx) {
+    if (userSeq[idx] === gameSeq[idx]) {
+        if (userSeq.length === gameSeq.length) {
+            setTimeout(levelUp, 1000);
+        }
+    } else {
+        h2.innerHTML = `Game Over! <br> Score: <b>${level - 1}</b><br>Press any key to restart`;
+        document.body.style.backgroundColor = "red";
+        setTimeout(() => document.body.style.backgroundColor = "#f4f4f4", 300);
+        reset();
+    }
+}
+
+function btnPress() {
+    if (!started) return;
+
+    let btn = this;
+    userFlash(btn);
+
+    let userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    checkAnswer(userSeq.length - 1);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for (let btn of allBtns) {
+    btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
